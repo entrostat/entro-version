7b17916ca341fccac36aca11a59c34dd01c71383
Kerren <kerren@entrostat.com>
2021-09-20 14:39:25 +0200

    Setting up the repo with standard-version and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 class EntroVersion extends Command {
-  static description = 'describe the command here'
+    static description = 'Calculates the version using standard-version and a release using git-flow';
 
-  static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    static flags = {};
 
-  static args = [{name: 'file'}]
+    static args = [];
 
-  async run() {
-    const {args, flags} = this.parse(EntroVersion)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    async run() {
+        const { args, flags } = this.parse(EntroVersion);
     }
-  }
 }
 
-export = EntroVersion
+export = EntroVersion;
