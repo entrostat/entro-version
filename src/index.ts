ab7628ef98a9f3526312375d2f28dfa457dbb34c
Kerren <kerren@entrostat.com>
2021-09-20 16:38:16 +0200

    fix(release): Trying to run releases non-interactively

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ class EntroVersion extends Command {
             await executeCommand(flags['during-release-post-hook'], this.log, this.error);
         }
 
-        await executeCommand(`git flow release finish ${newVersion} -f`, this.log, this.error);
+        await executeCommand(`export GIT_MERGE_AUTOEDIT=no && git flow release finish ${newVersion}`, this.log, this.error);
         // await executeCommand(`git checkout ${flags['develop-branch-name']}`, this.log, this.error);
         //
         // if (!flags['no-push']) {
