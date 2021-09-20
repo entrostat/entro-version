b22ad33e98cf0576303108b61265439f8792bf10
Kerren <kerren@entrostat.com>
2021-09-20 15:25:17 +0200

    Initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,70 @@
 import { Command, flags } from '@oclif/command';
+import { executeCommand } from './execute-command';
 
 class EntroVersion extends Command {
     static description = 'Calculates the version using standard-version and a release using git-flow';
 
-    static flags = {};
+    static flags = {
+        'during-release-hook-pre': flags.string({
+            char: 'p',
+            description: 'Any commands to run during the release, before standard-version',
+        }),
+        'during-release-hook-post': flags.string({
+            char: 'P',
+            description: 'Any commands to run during the release, after standard-version',
+        }),
+        'no-sign': flags.boolean({
+            char: 's',
+            description: 'Do not sign during the release using your GPG key',
+            default: false,
+        }),
+    };
 
     static args = [];
 
     async run() {
         const { args, flags } = this.parse(EntroVersion);
+        const dryRunOutput = await this.getStandardVersionDryRunOutput();
+        const tagVersionRegex = /tagging release (v\d+\.\d+\.\d+)/gim;
+        const newVersion = (tagVersionRegex.exec(dryRunOutput) || [])[1];
+
+        await executeCommand(`git flow release start ${newVersion}`, this.log, this.warn);
+        if (flags['during-release-hook-pre']) {
+            await executeCommand(flags['during-release-hook-pre'], this.log, this.error);
+        }
+
+        if (flags['no-sign']) {
+            await executeCommand('standard-version', this.log, this.warn);
+        } else {
+            await executeCommand('standard-version --sign', this.log, this.warn);
+        }
+
+        if (flags['during-release-hook-post']) {
+            await executeCommand(flags['during-release-hook-post'], this.log, this.error);
+        }
+
+        try {
+            await executeCommand(`git flow release finish ${newVersion}`, this.log, this.warn);
+        } catch (e) {
+            await executeCommand(`git branch -d release/${newVersion}`, this.log, this.warn);
+        }
+    }
+
+    private async standardVersionExists() {
+        try {
+            await executeCommand('which standard-version', this.log, this.warn);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    private async getStandardVersionDryRunOutput() {
+        if (await this.standardVersionExists()) {
+            return await executeCommand('standard-version --dry-run', this.log, this.warn);
+        } else {
+            return await executeCommand('npx standard-version --dry-run', this.log, this.warn);
+        }
     }
 }
 
